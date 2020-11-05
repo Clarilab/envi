bea0f538c1af7fc5e7ff28b84c5812785268656e
Philipp Hirsch <ph@fino.digital>
2020-11-05 14:23:31 +0100

    more verbose error message

--- error.go
+++ error.go
@@ -1,10 +1,19 @@
 package envi
 
+import (
+	"fmt"
+	"strings"
+)
+
 // RequiredEnvVarsMissing says, that a required Environment Variable is not given.
 type RequiredEnvVarsMissing struct {
 	MissingVars []string
 }
 
 func (e *RequiredEnvVarsMissing) Error() string {
-	return "One or more required environment variables are missing"
+	return fmt.Sprintf("One or more required environment variables are missing\nThe missing variables are: %s", e.printMissingVars())
+}
+
+func (e *RequiredEnvVarsMissing) printMissingVars() string {
+	return strings.Join(e.MissingVars, ", ")
 }
