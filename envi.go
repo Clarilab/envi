fb6067d1c5889e92198e49f515346db78f26c338
Maximilian Breida <maxbreida@gmail.com>
2024-05-22 18:53:11 +0200

    feat(WIP): add filewatcher functionality

--- envi.go
+++ envi.go
@@ -7,7 +7,9 @@ import (
 	"os"
 	"reflect"
 	"strings"
+	"sync"
 
+	"github.com/fsnotify/fsnotify"
 	"gopkg.in/yaml.v3"
 )
 
@@ -15,6 +17,11 @@ const tagDefault = "default"
 const tagEnv = "env"
 const tagType = "type"
 const tagRequired = "required"
+const tagWatch = "watch"
+
+type FileWatcher interface {
+	Notify()
+}
 
 // here could be a large description
 func GetEnvs(e any) error {
@@ -30,6 +37,7 @@ func GetEnvs(e any) error {
 
 	for i := 0; i < v.NumField(); i++ {
 		field := v.Field(i)
+		field = resolveValuePointer(field)
 
 		defaultTag := getStructTag(t.Field(i), tagDefault)
 		envTag := getStructTag(t.Field(i), tagEnv)
@@ -41,23 +49,34 @@ func GetEnvs(e any) error {
 		switch field.Kind() {
 		case reflect.Struct:
 			typeTag := getStructTag(t.Field(i), tagType)
+			watchTag := getStructTag(t.Field(i), tagWatch)
 
 			path := cmp.Or(os.Getenv(envTag), defaultTag)
 			typeVal := cmp.Or(typeTag, "yaml")
 
-			blob, err := os.ReadFile(path)
-			if err != nil {
-				return fmt.Errorf("error reading file: %w", err)
-			}
-
 			switch typeVal {
 			case "yaml", "yml":
-				err := loadFile(field, blob, yaml.Unmarshal)
+				err := loadFile(field, path, yaml.Unmarshal)
 				if err != nil {
 					return fmt.Errorf("error unmarshaling yaml: %w", err)
 				}
+
+				if watchTag == "true" {
+					watcher, err := fsnotify.NewWatcher()
+					if err != nil {
+						return fmt.Errorf("failed to init watcher: %w", err)
+					}
+
+					go fileWatcher(watcher, field, path, yaml.Unmarshal)
+
+					err = watcher.Add(path)
+					if err != nil {
+						watcher.Close()
+						return fmt.Errorf("failed to add path to watcher: %w", err)
+					}
+				}
 			case "json":
-				err := loadFile(field, blob, json.Unmarshal)
+				err := loadFile(field, path, json.Unmarshal)
 				if err != nil {
 					return fmt.Errorf("error unmarshaling json: %w", err)
 				}
@@ -72,7 +91,7 @@ func GetEnvs(e any) error {
 					return nil
 				}
 
-				err := loadFile(field, blob, unmarshal)
+				err := loadFile(field, path, unmarshal)
 				if err != nil {
 					return fmt.Errorf("error unmarshaling text: %w", err)
 				}
@@ -101,16 +120,16 @@ func GetEnvs(e any) error {
 	return nil
 }
 
-func loadFile(field reflect.Value, blob []byte, unmarshal func([]byte, any) error) error {
-	castedVal := reflect.New(field.Type())
+func loadFile(field reflect.Value, path string, unmarshal func([]byte, any) error) error {
+	blob, err := os.ReadFile(path)
+	if err != nil {
+		return fmt.Errorf("error reading file: %w", err)
+	}
 
-	err := unmarshal(blob, castedVal.Interface())
+	err = unmarshal(blob, field.Addr().Interface())
 	if err != nil {
 		return fmt.Errorf("could not unmarshal: %w", err)
 	}
-
-	castedVal = resolveValuePointer(castedVal)
-	field.Set(castedVal)
 
 	return nil
 }
@@ -160,4 +179,54 @@ func resolveTypePointer(rt reflect.Type) reflect.Type {
 
 func getStructTag(f reflect.StructField, tagName string) string {
 	return f.Tag.Get(tagName)
+}
+
+func fileWatcher(
+	watcher *fsnotify.Watcher,
+	field reflect.Value,
+	filePath string,
+	unmarshal func([]byte, any) error,
+) {
+	callback, ok := field.Addr().Interface().(FileWatcher)
+	if !ok {
+		fmt.Println("callback is not of type FileWatcher")
+		return
+	}
+
+	mutex := new(sync.Mutex)
+
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return
+			}
+
+			if event.Has(fsnotify.Chmod) || event.Has(fsnotify.Write) {
+				mutex.Lock()
+				err := loadFile(field, filePath, unmarshal)
+				if err != nil {
+					// watchErrChan <- fmt.Errorf("error reloading watched file: %w", err)
+					fmt.Println("error reloading watched file: %w", err)
+					continue
+				}
+				mutex.Unlock()
+
+				callback.Notify()
+			} else if event.Has(fsnotify.Remove) {
+				err := watcher.Add(filePath)
+				if err != nil {
+					// watchErrChan <- fmt.Errorf("error reenabling watcher for file: %w", err)
+					fmt.Println("error reenabling watcher for file: %w", err)
+				}
+			}
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return
+			}
+
+			// watchErrChan <- fmt.Errorf("error while watching file: %w", err)
+			fmt.Println("error while watching file: %w", err)
+		}
+	}
 }
--- envi_test.go
+++ envi_test.go
@@ -1,27 +1,74 @@
 package envi_test
 
 import (
+	"fmt"
+	"os"
+	"sync"
 	"testing"
 
 	"github.com/Clarilab/envi/v2"
 	"github.com/davecgh/go-spew/spew"
 )
 
+type MightyConfig struct {
+	WaitGroup *sync.WaitGroup
+	Name      string   `yaml:"PETER"`
+	Tenants   []string `yaml:"TENANTS"`
+}
+
+type Env struct {
+	MightyConfig MightyConfig `env:"ENVI_TEST" watch:"true"`
+}
+
+func (m MightyConfig) Notify() {
+	m.WaitGroup.Done()
+}
+
 func Test_Basic(t *testing.T) {
-	t.Setenv("ENVI_TEST", "/Users/lks/development/go/playground/mighty.yml")
+	t.Setenv("ENVI_TEST", "/Users/maxbreida/dev/github.com/Clarilab/envi/test.yaml")
 
-	type MightyConfig struct {
-		Name    string   `yaml:"PETER"`
-		Tenants []string `yaml:"TENANTS"`
+	env := Env{
+		MightyConfig: MightyConfig{
+			WaitGroup: new(sync.WaitGroup),
+		},
 	}
 
-	type Env struct {
-        MightyConfig MightyConfig `env:"ENVI_TEST"`
+	if err := os.WriteFile(
+		"test.yaml",
+		[]byte(fmt.Sprintf("%s: %s", "PETER", "PAN")),
+		0o664,
+	); err != nil {
+		t.Fatal(err)
 	}
 
-	env := Env{}
+	t.Cleanup(func() {
+		if err := os.Remove("test.yaml"); err != nil {
+			t.Fatal(err)
+		}
+	})
 
-	envi.GetEnvs(&env)
+	err := envi.GetEnvs(&env)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	spew.Dump(env)
+
+	env.MightyConfig.WaitGroup.Add(2)
+
+	if err := os.WriteFile(
+		"test.yaml",
+		[]byte(fmt.Sprintf("%s: %s", "PETER", "PANUS")),
+		0o664,
+	); err != nil {
+		t.Fatal(err)
+	}
+
+	env.MightyConfig.WaitGroup.Wait()
+
+	if env.MightyConfig.Name != "PANUS" {
+		t.Fatal("expected PANUS")
+	}
 
 	spew.Dump(env)
 }
